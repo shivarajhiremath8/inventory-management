4d0009a9091e434846ff39a2c0728b6ecf091584
Shivaraj Hiremath <shivaraj94826@gmail.com>
2025-11-03 22:39:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+
+    return (
+        <div className="bg-black text-white min-h-screen">
+            <h1 className="text-xl font-bold text-center p-4 font-mono">
+                Initial Setup
+            </h1>
+        </div>
+    )
+}
